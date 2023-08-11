89ce106c3038027134c4690eafa6324993e9c985
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-11 23:53:35 +0100

    6-numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all numbers from 0 to 9 using putchar
+ * Return: Always (success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar(i+'0');
+}
+
+putchar('\n');
+return (0);
+}
