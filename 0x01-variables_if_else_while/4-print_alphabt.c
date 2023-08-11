8648cc540ef6ac8322f4e393c7e679aeac157a82
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-11 23:32:05 +0100

    4 print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the alphabets from a to z
+ * Return: Always (success)
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+
+putchar('\n');
+return (0);
+}
