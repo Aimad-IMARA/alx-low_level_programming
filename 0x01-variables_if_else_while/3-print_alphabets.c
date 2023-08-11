100dde376ce76a5ab687de2e922ac608dda739d7
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-11 23:29:40 +0100

    Adding A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the alphabets from a to z
+ * Return: Always (success)
+ */
+int main(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        putchar(c);
+    }
+    for (c = 'A'; c <= 'Z'; c++)
+    {
+        putchar(c);
+    }
+
+    putchar('\n');
+    return (0);
+}
