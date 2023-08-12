71f0d78e611ff6dafbc3821f3ecbe1e8570bd385
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-12 03:42:41 +0100

    All tasks completed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,12 +13,12 @@ for (j = 0; j <= 8; j++)
 {
 for (k = 0; k <= 9; k++)
 {
-for (l = 0; l <= 9; l++)
+for (l = 1; l <= 9; l++)
 {
 putchar(i + '0');
 putchar(j + '0');
 putchar(' ');
-putchar(k + '0'); 
+putchar(k + '0');
 putchar(l + '0');
 if (i != 9 || j != 8 || k != 9 || l != 9)
 {
