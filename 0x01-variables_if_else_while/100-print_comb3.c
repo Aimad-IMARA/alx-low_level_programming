b0ef508e35ffe4d86946e9a246eb85fb28350b43
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-12 03:06:20 +0100

    this is the last update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,13 @@ for (i = 0; i < 9; i++)
 {
 for (j = i+1 ; j <= 9;j++)
 {
-    putchar(i + '0');
-    putchar(j + '0');
-    putchar(',');
-    putchar(' ');
+putchar(i + '0');
+putchar(j + '0');
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
 }
 }
 
