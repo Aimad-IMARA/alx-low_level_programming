fd6ce49946686497c62ed2586891b5d6ca474312
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-23 00:43:12 +0100

    betty code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 
 /**
  * reset_to_98 - change the value of the object pointed to 98
- *
+ * @n : pointer to change
  * Return: nothing.
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+*n = 98;
 }
