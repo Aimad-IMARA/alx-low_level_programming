9c5de9572889b0400faf041c65c87cf078f0eb8a
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-23 02:37:25 +0100

    5 assigne

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ * Return: nothing.
+ */
+void print_rev(char *s)
+{
+int i;
+i = 0;
+while (*s != '\0')
+{
+i++;
+s++;
+}
+while (i != 0)
+{
+putchar(s[i]);
+i--;
+}
+putchar('\n');
+}
