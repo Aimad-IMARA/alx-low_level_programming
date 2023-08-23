5370a2ccd9380a8708a2769f0eb2f1a2cd85b9ec
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-23 01:26:32 +0100

    2 assignement

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int : swap two pointers values
+ * @a : pointer 
+ * @b : pointer
+ * Return: nothing.
+ */
+void swap_int(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
