e9b09a4ad40f88471dee202c7dbd6ef9aaee7236
Aimad-IMARA <aimadimara99@gmail.com>
2023-08-31 01:23:53 +0100

    Done 0_recru

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,21 +2,18 @@
 /**
  * _puts_recursion - print a string
  * @s:string
- * Return: void
- * 
+ * Return:void
  */
-
 void _puts_recursion(char *s)
 {
 if (*s)
 {
-_putchar(*s);
-_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 else
 {
-_putchar('\n');
+	_putchar('\n');
 }
 
 }
-
